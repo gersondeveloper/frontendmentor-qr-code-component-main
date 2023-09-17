f16fe50b2e1d0ed963852f819c6fefaae617427c
Gerson C Filho <gersoncfilho@mac.com>
2023-09-17 08:48:06 +0100

    update README.md file

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+export default {
+  publicPath: '/frontendmentor-qr-code-component-main'
+};
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
